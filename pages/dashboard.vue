cbb4cd1a525ccaaa2ea2caadcb3b06ca464addfb
Mohamed Ehab <63422773+mohehab@users.noreply.github.com>
2021-06-27 12:53:38 +0200

    Add files via upload

---
+++ pages/dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col sm:flex-row h-screen">
+    <sideBar></sideBar>
+    <div class="flex-grow flex flex-col">
+      <HeaderCom></HeaderCom>
+      <TrackerCom></TrackerCom>
+      <FooterCom></FooterCom>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex m-auto justify-center text-center items-center h-screen">
+    <div class="flex-none w-8 md:w-40"></div>
+    <div class="flex-grow">
+      <img src="@/assets/imgs/mytigate-logo.svg" class="mb-10" />
+      <NuxtLink
+        to="/dashboard"
+        class="
+          border-solid border-2
+          rounded-lg
+          p-2
+          sm:p-3
+          text-lg
+          sm:text-2xl
+          border-blue-400
+          text-blue-900
+          font-bold
+          uppercase
+        "
+        >Enter our Dashboard</NuxtLink
+      >
+    </div>
+    <div class="flex-none w-8 md:w-40"></div>
+  </div>
+</template>
